df62e77d84493883fefd2ff5ecea313516296143
Juamez <juanmendezs.a@gmail.com>
2022-03-23 01:54:51 -0500

    add new routes from corresponding recently added pages

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import '../styles/global.scss'
 
-import Login from '../containers/Login'
-import Layout from '../containers/Layout'
-import RecoveryPassword from '../containers/RecoveryPassword'
-import Home from '../pages/Home.jsx'
-import NotFound from '../pages/NotFound'
+import AppContext from '../context/AppContext'
+
+import Login from '@containers/Login'
+import Layout from '@containers/Layout'
+
+import Checkout from '@pages/Checkout'
+import CreateAccount from '@pages/CreateAccount'
+import Home from '@pages/Home.jsx'
+import MyAccount from '@pages/MyAccount'
+import NewPassword from '@pages/NewPassword'
+import NotFound from '@pages/NotFound'
+import Orders from '@pages/Orders'
+import PasswordRecovery from '@pages/PasswordRecovery'
+import SendEmail from '@pages/SendEmail'
 
 const App = () => {
   return (
@@ -14,8 +23,14 @@ const App = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />}/>
-          <Route path="/login" element={<Login />} />  
-          <Route path="/recovery-password" element={<RecoveryPassword />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/password-recovery" element={<PasswordRecovery />} />
+          <Route path="/send-email" element={<SendEmail />} />
+          <Route path="/new.password" element={<NewPassword />} />
+          <Route path="/account" element={<MyAccount />} />
+          <Route path="/signup" element={<CreateAccount />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/orders" element={<Orders />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
