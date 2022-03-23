70ba8c6791d081459e56ac19f4eeec56641cb317
Juamez <juanmendezs.a@gmail.com>
2022-03-22 21:30:09 -0500

    add menu component and style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../styles/Header.scss'
+import '@styles/Header.scss'
 
 import menu from '@icons/icon_menu.svg'
 import shoppingCart from '@icons/icon_shopping_cart.svg'
